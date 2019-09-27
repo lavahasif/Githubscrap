aef5e8cee6673c6da78c14347ed7432ca2267340
Hasif <lavahasif@gmail.com>
2019-09-27 21:01:25 +0530

    list page wise

--- venv/github/__init__.py
+++ venv/github/__init__.py
@@ -1,11 +1,12 @@
-# only for learning purpose
-
 import pandas as pd
+import requests
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
+ii = 1
 repository = []
 projects_name = []
+mylink = []
 projects = []
 downloadlink = []
 githublink = 'https://github.com'
@@ -34,10 +35,61 @@ def getdwonloadlink():
 
             for a in soup.findAll('div', attrs={'class': 'mt-2'}):
                 tag = githublink + a.find('a', attrs={'class': 'btn'}).attrs['href']
+                mylink.append(tag)
             print(f'{projects_name[i]}===>', tag)
         except:
             print("error")
 
 
-getrepository()
+def download(url, file_name):
+    get_response = requests.get(url, stream=True)
+    file_name = url.split("/")[-3] + ".zip"
+    with open(file_name, 'wb') as f:
+        for chunk in get_response.iter_content(chunk_size=1024):
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
+
+
+def downloadlink():
+    for url in range(len(mylink)):
+        download(mylink[url], projects_name[url])
+
+
+def setrepository(url):
+    if len(url) > 2:
+        repository.clear()
+        repository.append(url)
+        recusiveappend(url)
+    else:
+        print("empty")
+    getrepository()
+
+
+def recursejump(url):
+    print(url)
+
+
+def recusiveappend(url):
+    # print(url)
+    html = urlopen(url).read().decode('utf-8')
+    soup = BeautifulSoup(html, features='lxml')
+    for a in soup.findAll('div', attrs={'class': 'BtnGroup'}):
+        try:
+            tag = a.find('a', attrs={'class': 'btn'}, string='Next').attrs['href']
+            name = a.find('a', attrs={'class': 'btn'}).text
+            if len(tag) > 3:
+                # print(tag)
+                if 'after' in tag:
+                    repository.append(tag)
+                    recusiveappend(tag)
+
+        except:
+            print("error")
+
+
+# recusiveappend('https://github.com/iampawan?tab=repositories')
+# for a in repository:
+#     print(a)
+setrepository('https://github.com/pkrawat1?tab=repositories')
 getdwonloadlink()
+downloadlink()
